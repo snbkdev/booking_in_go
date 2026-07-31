81e43781711427c9f49e30ad394518d01ce7061e
Asanbek09 <samudinua@gmail.com>
2026-07-31 10:44:20 +0600

    Работа над валидацией при заполнении формы

--- internal/forms/errors.go
+++ internal/forms/errors.go
@@ -7,7 +7,7 @@ func (e errors) Add(field, message string) {
 	e[field] = append(e[field], message)
 }
 
-// Greturns the first error message
+// Get returns the first error message
 func (e errors) Get(field string) string {
 	es := e[field]
 	if len(es) == 0 {
